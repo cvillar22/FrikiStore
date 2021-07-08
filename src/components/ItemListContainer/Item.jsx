ef9844886f40dab764a82f8d0f191bab399a2038
cvillar22 <csorrochevillar@gmail.com>
2021-07-08 18:04:44 -0300

    card del elemento item

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -2,28 +2,28 @@ import React from "react";
 
 const Item = ({ title, price, prodImg, stock,  descrip }) => {
   return (
-    <div className="card">
+    <article className="card has-text-centered">
       
-      <div className="card-image">
-        <figure className="image is-150x150">
+      <header className="card-header is-flex is-justify-content-center">
+        <figure className="card-image p-1">
           <img className="image" src={prodImg} alt={title} />
         </figure>
-      </div>
+      </header>
 
-      <div className="card-content">
-        <h2 className="subtitle">{title}</h2>
-        <b className="is-block"> ${price} </b>
+      <section className="card-content">
+        <h2 className="subtitle mb-1 is-size-3-mobile is-size-4-desktop">{title}</h2>
+        <b className="is-block mb-1 is-size-4-mobile is-size-5-desktop"> ${price} </b>
         <small className="tag is-info is-light">stock: {stock}</small>
-      </div>
+      </section>
 
-      <footer className="card-footer">
+      <footer className="card-footer is-flex-direction-column is-align-items-center pb-2">
         {descrip ? descrip : ""}
         <button className="button" type="button" title="Add to cart">
           Add to cart
         </button>
       </footer>
 
-    </div>
+    </article>
   );
 };
 
